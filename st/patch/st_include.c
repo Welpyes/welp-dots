e93f414c716a482a8a1dfc6275d0281930de2350
Welpyes <helloilikeoppai@protonmail.com>
2026-03-04 11:10:46 +0800

    feat: ✨

---
+++ st/patch/st_include.c
@@ -0,0 +1,5 @@
+/* Patches */
+#include "externalpipe.c"
+#include "newterm.c"
+#include "reflow.c"
+#include "sync.c"
---
+++ st/patch/st_include.h
@@ -0,0 +1,5 @@
+/* Patches */
+#include "externalpipe.h"
+#include "newterm.h"
+#include "reflow.h"
+#include "sync.h"
