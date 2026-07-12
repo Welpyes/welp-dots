7f3d725409687a982816bb4a6f0f4c417f169bbd
Welpyes <helloilikeoppai@protonmail.com>
2026-07-12 22:41:14 +0800

    fix: nvim stuff

--- main-stuff/dot-config/nvim/init.lua
+++ main-stuff/dot-config/nvim/init.lua
@@ -4,4 +4,12 @@ require("config.lualine")
 require("options")
 require("mappings")
 require("file-maps")
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = function()
+    local hl = vim.api.nvim_get_hl(0, { name = "Comment" })
+    hl.italic = false
+    vim.api.nvim_set_hl(0, "Comment", hl)
+    vim.api.nvim_set_hl(0, "@comment", { link = "Comment" })
+  end,
+})
 vim.cmd [[colorscheme tokyonight-night]]
